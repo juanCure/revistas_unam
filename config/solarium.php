49a6c8608193e863d4b2ef9f98f1f9e5ecebb61d
Juan Manuel Rodríguez <manuel2489@gmail.com>
2022-04-19 11:42:08 -0500

    Cambiando la IP del vufind

--- config/solarium.php
+++ config/solarium.php
@@ -3,8 +3,7 @@
 return [
 	'endpoint' => [
 		'biblio' => [
-			// 'host' => env('SOLR_HOST', '132.248.204.81'),
-			'host' => env('SOLR_HOST', '132.248.204.101'),
+			'host' => env('SOLR_HOST', '132.248.204.99'),
 			'port' => env('SOLR_PORT', '8983'),
 			'path' => env('SOLR_PATH', ''),
 			'core' => env('SOLR_CORE', 'biblio'),
