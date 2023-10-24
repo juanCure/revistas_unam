0afce5fcc71b7e219a660a717bb998b1d437d6ef
Juan Manuel Rodríguez <manuel2489@gmail.com>
2023-10-23 18:28:07 -0600

    Retirando Google Analytics UA

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -184,17 +184,6 @@
                           </ul>
                       </div>
                   </div>
-                  {{-- Agregando Google Analytics  --}}
-                  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-15759172-1"></script>
-                  <script>
-                      window.dataLayer = window.dataLayer || [];
-                      function gtag(){
-                        dataLayer.push(arguments);
-                      }
-                      gtag('js', new Date());
-
-                      gtag('config', 'UA-15759172-1');
-                  </script>
               </div>
           </footer>
         </div>
